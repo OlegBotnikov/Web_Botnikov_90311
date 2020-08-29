a95e4b2bcc6991a6416cd0251cd919e191c64827
botnikov <botnikov@mail.ru>
2020-08-29 21:54:48 +0300

    Lab 3 законченная

---
+++ Web_Botnikov_90311/Components/MenuViewComponent.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Botnikov_90311.Models;
+
+namespace Web_Botnikov_90311.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        // Инициализация списка элементов меню
+        private List<MenuItem> _menuItems = new List<MenuItem>
+        {
+            new MenuItem{ Controller="Home", Action="Index", Text="Lab 2"},
+            new MenuItem{ Controller="Product", Action="Index",
+            Text="Каталог"},
+            new MenuItem{ IsPage=true, Area="Admin", Page="/Index",
+            Text="Администрирование"},
+            new MenuItem{ IsPage=true, Area="Admin", Page="/Index",
+            Text="Ботников"}
+        };
+
+        public IViewComponentResult Invoke()
+        {
+            //Получение значений сегментов маршрута
+            var controller = ViewContext.RouteData.Values["controller"];
+            var page = ViewContext.RouteData.Values["page"];
+            var area = ViewContext.RouteData.Values["area"];
+            foreach (var item in _menuItems)
+            {
+                // Название контроллера совпадает?
+                var _matchController = controller?.Equals(item.Controller)
+                ?? false;
+                // Название зоны совпадает?
+                var _matchArea = area?.Equals(item.Area) ?? false;
+                // Если есть совпадение, то сделать элемент меню активным
+                // (применить соответствующий класс CSS)
+                if (_matchController || _matchArea)
+                {
+                    item.Active = "active";
+                }
+            }
+            return View(_menuItems);
+        }
+    }
+}
