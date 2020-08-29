677026d290be5bc03bab1706a65c88b387c39eb5
botnikov <botnikov@mail.ru>
2020-08-29 20:09:56 +0300

    Лаб 2 рабочая

--- Web_Botnikov_90311/Controllers/HomeController.cs
+++ Web_Botnikov_90311/Controllers/HomeController.cs
@@ -5,33 +5,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Web_Botnikov_90311.Models;
 
 namespace Web_Botnikov_90311.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
             return View();
         }
-
-        public IActionResult Privacy()
+        private List<ListDemo> _listDemo;
+        public class ListDemo
         {
-            return View();
+            public int ListItemValue { get; set; }
+            public string ListItemText { get; set; }
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public HomeController()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            _listDemo = new List<ListDemo>
+            {
+                new ListDemo{ ListItemValue=1, ListItemText="Элемент 1"},
+                new ListDemo{ ListItemValue=2, ListItemText="Элемент 2"},
+                new ListDemo{ ListItemValue=3, ListItemText="Элемент 3"},
+                new ListDemo{ ListItemValue=4, ListItemText="Элемент 4"}
+            };
         }
+
     }
 }
